3b43ef5cbedd5d136fb11ad025b4217adf00aae6
AtharvaDhumal2003 <atharvadh2312@gmail.com>
2025-11-22 23:47:23 +0530

    Assignment_30(Q1-Q5) solutions

---
+++ Assignments/Assignment_30/program1_3.c
@@ -0,0 +1,65 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+//  Required Header File
+//  
+/////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+/////////////////////////////////////////////////////////////////////////////
+//  Function name:  Pattern
+//  Description:    It is used to Display number matrix of given row 
+//                  and columns.                 
+//  Input:          int,int
+//  Author:         Atharva Sanjay Dhumal.
+//  Date:           20/11/2025
+//
+/////////////////////////////////////////////////////////////////////////////
+void Pattern(int iRow,int iCol)
+{
+    int i=0,j= 0;
+
+    for(i =1; i<=iRow;i++)
+    {
+        for(j=1;j<=iCol;j++)
+        {
+           if(i == j)
+           {
+             printf("$\t");
+           }
+           else
+           {
+             printf("*\t");
+           } 
+        }
+        printf("\n");
+    }
+}
+/////////////////////////////////////////////////////////////////////////////
+//  
+//  Entry Point Function for the application
+//
+/////////////////////////////////////////////////////////////////////////////
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of Rows:\n");
+    scanf("%d",&iValue1);
+
+    
+    printf("Enter number of Columns:\n");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+    return 0;
+}
+////////////////////////////////////////////////////////////////////////////////////
+//
+// Enter number of Rows:
+// 4
+// Enter number of Columns:
+// 4
+// $       *       *       *
+// *       $       *       *
+// *       *       $       *
+// *       *       *       $
+/////////////////////////////////////////////////////////////////////////////////////
