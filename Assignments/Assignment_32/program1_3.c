15fbabc8e359b1e3bfab5e47a61a3a9462fd66f8
AtharvaDhumal2003 <atharvadh2312@gmail.com>
2025-11-24 01:54:49 +0530

    Assignment_32(Q1_Q5)solutions

---
+++ Assignments/Assignment_32/program1_3.c
@@ -0,0 +1,76 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+//  Required Header File
+//  
+/////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+/////////////////////////////////////////////////////////////////////////////
+//  Function name:  Pattern
+//  Description:    It is used to Display number matrix of given row 
+//                  and columns.                 
+//  Input:          int,int
+//  Author:         Atharva Sanjay Dhumal.
+//  Date:           23/11/2025
+//
+/////////////////////////////////////////////////////////////////////////////
+void Pattern(int iRow,int iCol)
+{
+    int i=0,j= 0;
+    
+    if(iRow != iCol)
+    {
+        printf("Number of Rows and Columns must be same.\n");
+        return;
+    }
+
+    for(i = 1; i<= iRow; i++)
+    {
+        for(j =1; j<= iCol; j++)
+        {
+            if((i + j) == (iRow+1) || i == 1 || i == iRow || j == 1 || j == iCol)
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf(" \t");
+            }
+        }
+        printf("\n");
+    }
+   
+
+}
+/////////////////////////////////////////////////////////////////////////////
+//  
+//  Entry Point Function for the application
+//
+/////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of Rows:\n");
+    scanf("%d",&iValue1);
+
+    
+    printf("Enter number of Columns:\n");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+    return 0;
+}
+////////////////////////////////////////////////////////////////////////////////////
+//
+//Enter number of Rows:
+//6
+//Enter number of Columns:
+//6
+//*       *       *       *       *       *
+//*                               *       *
+//*                       *               *
+//*               *                       *
+//*       *                               *
+//*       *       *       *       *       *
+////////////////////////////////////////////////////////////////////////////////////
---
+++ Assignments/Assignment_32/program1_5.c
@@ -0,0 +1,75 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+//  Required Header File
+//  
+/////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+/////////////////////////////////////////////////////////////////////////////
+//  Function name:  Pattern
+//  Description:    It is used to Display number matrix of given row 
+//                  and columns.                 
+//  Input:          int,int
+//  Author:         Atharva Sanjay Dhumal.
+//  Date:           23/11/2025
+//
+/////////////////////////////////////////////////////////////////////////////
+void Pattern(int iRow,int iCol)
+{
+    int i = 0, j = 0;
+    if(iRow != iCol)
+    {
+        printf("Number of Rows and Columns must be same.");
+        return;
+    }
+
+    for(i = 1; i<= iRow;i++)
+    {
+        for(j = 1; j<=iCol;j++)
+        {
+            if(i == j || i == 1 || i == iRow || j == 1|| j == iCol)
+            {
+                printf("%d\t",j);
+            }
+            else
+            {
+                printf(" \t");
+            }
+        }
+        printf("\n");
+    }
+}
+/////////////////////////////////////////////////////////////////////////////
+//  
+//  Entry Point Function for the application
+//
+/////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of Rows:\n");
+    scanf("%d",&iValue1);
+
+    
+    printf("Enter number of Columns:\n");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+    return 0;
+}
+////////////////////////////////////////////////////////////////////////////////////
+//
+//Enter number of Rows:
+//6
+//Enter number of Columns:
+//6
+//1       2       3       4       5       6
+//1       2                               6
+//1               3                       6
+//1                       4               6
+//1                               5       6
+//1       2       3       4       5       6
+////////////////////////////////////////////////////////////////////////////////////
+
+
