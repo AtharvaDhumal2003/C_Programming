ecf421bd214f0e53e06710e35dc2ba2682722bb2
AtharvaDhumal2003 <atharvadh2312@gmail.com>
2025-12-05 10:34:53 +0530

    Update Program1_1.c

--- Assignments/Assignment_33/Program1_1.c
+++ Assignments/Assignment_33/Program1_1.c
@@ -7,7 +7,7 @@
 
 /////////////////////////////////////////////////////////////////////////////
 //  Function name:  Display
-//  Description:    It use to check whether given character is alphabet or not.
+//  Description:    It use to display ASCII table.
 //  Input:          char
 //  Output:         boolean
 //  Author:         Atharva Sanjay Dhumal.
@@ -20,7 +20,10 @@ void DisplayASCII()
     for(ch = '\0'; ch <' ÿ'; ch++)
     {
 
-        printf("%c\n",ch);
+        printf("|%c|\n",ch);
+        printf("|%d|\n",ch);
+        printf("|%#X|\n",ch);
+        printf("|%o|\n",ch);
     }
 }
 /////////////////////////////////////////////////////////////////////////////
@@ -36,10 +39,12 @@ int main()
 }
 ////////////////////////////////////////////////////////////////////////////////////
 //
-//Enter the character :
-//c
-//It is a Cahracter
-//Enter the character :
-//%
-//It is not a Cahracter
+//|Symbol:=|      |Decimal:61|    |Hexadecimal:0X3D|      |Octal:75       |
+//|Symbol:>|      |Decimal:62|    |Hexadecimal:0X3E|      |Octal:76       |
+//|Symbol:?|      |Decimal:63|    |Hexadecimal:0X3F|      |Octal:77       |
+//|Symbol:@|      |Decimal:64|    |Hexadecimal:0X40|      |Octal:100      |
+//|Symbol:A|      |Decimal:65|    |Hexadecimal:0X41|      |Octal:101      |
+//|Symbol:B|      |Decimal:66|    |Hexadecimal:0X42|      |Octal:102      |
+//|Symbol:C|      |Decimal:67|    |Hexadecimal:0X43|      |Octal:103      |
+//
 ////////////////////////////////////////////////////////////////////////////////////
